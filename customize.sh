9e4fdc8674e7418848b19343a444876e806e99d3
Rene <rrr333nnn333@gmail.com>
2026-03-23 22:24:39 -0400

    add: hot install support
    Co-authored-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- customize.sh
+++ customize.sh
@@ -3,6 +3,9 @@ DEST_BIN_DIR=/data/adb/ksu/bin
 SUSFS_BIN=/data/adb/ksu/bin/susfs
 PERSISTENT_DIR=/data/adb/brene
 
+# Hot Install
+export MODULE_HOT_INSTALL_REQUEST="true"
+
 # Disable outdated modules
 # echo "[✅] Disabling outdated modules"
 # modules="
